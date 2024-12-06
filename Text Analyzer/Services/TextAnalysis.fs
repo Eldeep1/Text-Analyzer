d59423dbbf4126ff39a171d48f8cc5e5c310683a
AhmedEbrahim2322004 <ahmed2322004ahmed@gmail.com>
2024-12-07 01:13:59 +0200

    added analyzeText function

--- Text Analyzer/Services/TextAnalysis.fs
+++ Text Analyzer/Services/TextAnalysis.fs
@@ -3,17 +3,78 @@
 open System.Text.RegularExpressions
 open System
 
-let analyzeText text =
-    let wordCount = Regex.Matches(text, @"\b\w+\b").Count
-    let sentenceCount = Regex.Matches(text, @"[.!?]").Count
-    let paragraphCount = text.Split([|"\r\n"; "\n"|], StringSplitOptions.RemoveEmptyEntries).Length
-    let avgSentenceLength = 
-        if sentenceCount > 0 then (float wordCount / float sentenceCount).ToString("0.##")
-        else "N/A"
+(*  
+   variable -> word - > when we add space
+   variable -> sentence - > when we add '.'
+   variable -> paragraph - > when we add '\n'
+        - check if multiple '\n'
+   --------------------
+
+   word frequancy ??
+   - display max frequant word
+
+   ---------------------
+   average sentence length
+        - variable -> total number of words
+        - variable -> total number of sentences 
+        - avg sentence length = total number of words / total number of sentences 
+*)
+
+let analyzeText (txt : string) = 
+
+    let mutable  sentences = 0
+    let mutable  paragraphs = 0
+
+    let rec split (txt: string) (index: int) (word: string) (last_char : char) (word_list : string list) = 
+        if index < txt.Length then
+            let current = txt.[index]
+            let next_index =  index+1
+            if current >= 'a' && current <= 'z' || current >= 'A' && current <= 'Z' then
+                split txt next_index (word + string current) current word_list
+            else
+                match current with
+                    | '.'  ->
+                        if last_char <> current then
+                            sentences <- sentences + 1
+                    | '\n' ->
+                        if last_char <> current then
+                            paragraphs <- paragraphs + 1 
+                        if last_char <> '.' then
+                            sentences <- sentences + 1
+                        printfn "%c" last_char
+                    | _ ->
+                        ()
+                if word <> "" then
+                    split txt next_index "" current (word :: word_list)
+                else
+                    split txt next_index "" current word_list
+        else
+            word_list
+    
+    let word_list = split txt 0 "" txt.[0] []
+
+    let mostFrequencyWord,mostFrequencyCount = 
+        word_list 
+        |> List.map (fun str -> str.ToLower())
+        |> List.countBy id  
+        |> List.maxBy snd
+
+    let words = word_list.Length
+    let avgLength = words / sentences
+
+    printfn "words %i" words
+    printfn "sentences %i" sentences
+    printfn "paragraphs %i" paragraphs
+    printfn "mostFrequencyWord %s" mostFrequencyWord
+    printfn "mostFrequencyCount %i" mostFrequencyCount
+    printfn "avgLength %i" avgLength
 
     Map [
-        "Number of Words", wordCount.ToString()
-        "Number of Sentences", sentenceCount.ToString()
-        "Number of Paragraphs", paragraphCount.ToString()
-        "Average Sentence Length", avgSentenceLength
+        "Number of Words", words.ToString()
+        "Number of Sentences", sentences.ToString()
+        "Number of Paragraphs", paragraphs.ToString()
+        "Most Frequent Word",mostFrequencyWord.ToString()
+        "Most Frequency Count",mostFrequencyCount.ToString()
+        "Average Sentence Length", avgLength.ToString()
     ]
+
